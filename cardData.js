ddd12a0231c797e3e8c807a7f14d53c8c403ebe3
Hosamov <hosamov@hotmail.com>
2021-09-27 15:56:26 -0700

    initial commit

---
+++ cardData.js
@@ -0,0 +1,34 @@
+const devices = [
+  {
+    device: 'Rock',
+    win: 'Scissors',
+    lose: 'Paper',
+    assets: [
+      './images/card_rock.png',
+      './images/card_hand_rock.png',
+      './images/card_text_rock.png'
+    ]
+  },
+  {
+    device: 'Paper',
+    win: 'Rock',
+    lose: 'Scissors',
+    assets: [
+      './images/card_paper.png',
+      './images/card_hand_paper.png',
+      './images/card_text_paper.png'
+    ]
+  },
+  {
+    device: 'Scissors',
+    win: 'Paper',
+    lose: 'Rock',
+    assets: [
+      './images/card_scissors.png',
+      './images/card_hand_scissors.png',
+      './images/card_text_scissors.png'
+    ]
+  },
+];
+
+export default devices;
