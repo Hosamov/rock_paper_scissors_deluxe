1f07170f80c33bb34ff508c060e500bba640e81b
Hosamov <hosamov@hotmail.com>
2021-10-18 11:53:10 -0700

    Update game instructions for Classic game mode

--- js/gameInstructions.js
+++ js/gameInstructions.js
@@ -5,27 +5,24 @@ const instructions = [
       <hr>
       <h3>Rock Paper Scissors: Classic</h3>
         <p>
-          This mode presents the most classic approach to the original Rock Paper
-          Scissors, but uses cards.
+          This game mode represents the most classic approach to the original
+          Rock Paper Scissors we all know and love, but using cards.
         </p>
 
       <h4>How to Play</h4>
-        <p>1. To start the game, choose the deck size (18, 36, or 54) and
-          deal shuffled deck by clicking the <strong>Start Game</strong>
-          button.
+        <p>1. After selecting "Classic" mode, choose the deck size (18, 36, or 54)
+           and deal shuffled deck by clicking the <strong>Start Game</strong>
+           button.
         </p>
-        <p>Two cards will be added to the play area, face down.</p>
         <p>
-          2. Player must click/tap their own card (bottom card) to flip
-          the cards over and see the results.
+          2. Player must click/tap a card from their hand to play against the AI.
         </p>
         <p>
           3. The player with the winning card takes both cards (both
           cards will be automatically given to the winner of the round)
         </p>
         <p>
-          4. Play continues until one player holds all cards and wins the
-          "war".
+          4. Play continues until one player holds all cards and wins the game.
         </p>
       <br>
     <h4>A Note on tie rounds</h4>
@@ -36,7 +33,7 @@ const instructions = [
       <p>The tie pot will be awarded to the next winning player.</p>
       <p>
         If a tie occurs with a player's last card, they cannot play again
-        to win the tie and thus lose the "war".
+        to win the tie and thus lose the game.
       </p>
       <br>
     <h4>More Notes:</h4>
@@ -115,23 +112,17 @@ const instructions = [
       <hr>
       <h3>Rock Paper Scissors: Memory</h3>
         <p>
-          This game of chance is a mashup of Rock Paper Scissors, and the
-          card game War.
+          [Game mode information here]
         </p>
 
       <h4>How to Play</h4>
-        <p>1. To start the game, choose the deck size (18, 36, or 54) and
-          deal shuffled deck by clicking the <strong>Start Game</strong>
-          button.
-        </p>
-        <p>Two cards will be added to the play area, face down.</p>
-        <p>
-          2. Player must click/tap their own card (bottom card) to flip
-          the cards over and see the results.
+        <p>1. [Step One]
         </p>
         <p>
-          3. The player with the winning card takes both cards (both
-          cards will be automatically given to the winner of the round)
+          2. [Step Two]
+        </p>
+        <p>
+          3. [Step Three]
         </p>
         <p>
           4. Play continues until one player holds all cards and wins the
